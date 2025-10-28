83e01de034d9719b016732b17451e5ab82e7d5a0
Maxx Seminario <maxx.seminario.79@gmail.com>
2025-10-28 12:53:21 -0500

    Lecture dir cleanup

--- lectures/misc_lects/compile_lectures.bat
+++
@@ -1,8 +0,0 @@
-pdflatex -shell-escape lecture01.tex
-pdflatex -shell-escape lecture02.tex
-pdflatex -shell-escape lecture03.tex
-pdflatex -shell-escape lecture04.tex
-pdflatex -shell-escape lecture05.tex
-pdflatex -shell-escape lecture06.tex
-pdflatex -shell-escape lecture07.tex
-pdflatex -shell-escape lecture08.tex
